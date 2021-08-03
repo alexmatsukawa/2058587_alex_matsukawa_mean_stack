7a70eb16264276faed7d70df230d09dfced294e8
Alex Matsukawa <amatsukawa9@gmail.com>
2021-08-03 10:23:58 -0700

    completed budget planner

--- Phase 1/team-budget-planner/ft_func.js
+++ Phase 1/team-budget-planner/ft_func.js
@@ -1,13 +1,23 @@
 function displayData() {
     document.getElementById("main").innerHTML="";
+    var br = document.createElement("br");
+    document.getElementById("main").appendChild(br);
     let clientObj = localStorage.getItem("cliObj");
     console.log(clientObj);
     let clientJson = JSON.parse(clientObj);
     console.log(clientJson);
     var table = document.createElement("table");
+    table.setAttribute("border", "1px solid")
+    var r, c;
+    r = table.insertRow(0);
+    c = r.insertCell(0);
+    c.innerHTML = "Client Name";
+    c = r.insertCell(1);
+    c.innerHTML = "Project Name";
+    c = r.insertCell(2);
+    c.innerHTML = "Budget";
     for(var i = 0; i < clientJson.length; i++) {
-        var r, c;
-        r = table.insertRow(i);
+        r = table.insertRow(i + 1);
         c = r.insertCell(0);
         c.innerHTML = clientJson[i].cli_name;
         c = r.insertCell(1);
@@ -16,4 +26,11 @@ function displayData() {
         c.innerHTML = clientJson[i].budget;
     }
     document.getElementById("main").appendChild(table);
+    var p = document.createElement("p");
+    var budgetTotal = 0;
+    for(var j = 0; j < clientJson.length; j++) {
+        budgetTotal += parseInt(clientJson[j].budget);
+    }
+    p.innerHTML = "The total budget for all projects is " + budgetTotal + ".";
+    document.getElementById("main").appendChild(p);
 }
