17174a0cf30a251fc128944aa5641fbd91c4c816
Alex Matsukawa <amatsukawa9@gmail.com>
2021-08-31 13:58:31 -0700

    completed socket chatting functionality

--- Phase 3/socket-chatting/app.js
+++ Phase 3/socket-chatting/app.js
@@ -3,7 +3,6 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-var responses = ['How are you?', 'How was your day?', 'How may I help you?', 'What questions do you have for me?', 'Pleased to meet you. How may I help you today?', 'Sorry, I didn\'t quite get that. Could you please try again?'];
 app.get("/",(req,res)=> {
     res.sendFile(__dirname+"\\index.html");
 })
@@ -22,10 +21,6 @@ io.on("connection",(socket)=> {
         console.log("Time Sent: " + timestamp);
         console.log("");
     })
-    // sending data to client
-    var resIndex = Math.floor(Math.random() * responses.length);
-    //console.log(resIndex);
-    socket.emit("serv-msg", responses[resIndex]);
 })
 
 http.listen(8080,()=>console.log("Server running on port number 8080"));
