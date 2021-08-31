4433cb1bb9f2384866c9bac13fa40ba6e6eac4b6
Alex Matsukawa <amatsukawa9@gmail.com>
2021-08-31 12:22:00 -0700

    managed to get a server response to display; need to figure out why multiple won't display...

--- Phase 3/socket-chatting/app.js
+++ Phase 3/socket-chatting/app.js
@@ -3,7 +3,7 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-var responses = [{'response': 'How are you?'}, {'response': 'How was your day?'}, {'response': 'How may I help you?'}, {'response': 'What questions do you have for me?'}, {'response': 'Pleased to meet you. How may I help you today?'}, {'response': 'Sorry, I didn\'t quite get that. Could you please try again?'}];
+var responses = ['How are you?', 'How was your day?', 'How may I help you?', 'What questions do you have for me?', 'Pleased to meet you. How may I help you today?', 'Sorry, I didn\'t quite get that. Could you please try again?'];
 app.get("/",(req,res)=> {
     res.sendFile(__dirname+"\\index.html");
 })
@@ -20,11 +20,12 @@ io.on("connection",(socket)=> {
 
     socket.on("time-sent",(timestamp)=> {
         console.log("Time Sent: " + timestamp);
+        console.log("");
     })
     // sending data to client
     var resIndex = Math.floor(Math.random() * responses.length);
     //console.log(resIndex);
-    socket.emit("serv-msg", responses[resIndex].response.toString());
+    socket.emit("serv-msg", responses[resIndex]);
 })
 
 http.listen(8080,()=>console.log("Server running on port number 8080"));
