87ee714fd0d0d04c954358c7142418315a4f893b
Alex Matsukawa <amatsukawa9@gmail.com>
2021-09-06 10:05:28 -0700

    added app file

---
+++ Phase 3/mongodb-website/app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let routerCourse = require("./router/course.router");
+let app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/course", routerCourse);
+
+let url = "mongodb://localhost:27017/tcs_mean";
+
+mongoose.connect(url).then(res => console.log("Connected...")).catch(error => console.log(error));
+
+app.listen(8080, () => console.log("Server is running on port 8080..."));
